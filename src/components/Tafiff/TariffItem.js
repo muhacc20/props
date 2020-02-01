f1c558a731987050c1db922969b53d0e057861ec
Student <student@localhosts>
2020-02-01 15:11:52 +0500

    homepage configurated

--- src/components/Tafiff/TariffItem.js
+++ src/components/Tafiff/TariffItem.js
@@ -8,8 +8,6 @@ export default function TariffItem({ tariff }) {
   return (
     <>
       <div className='main' >
-
-
         <div className='block'>
           <p className="title"> {tariff.title} </p>
           <div className='price'>{tariff.price}</div>
@@ -18,20 +16,17 @@ export default function TariffItem({ tariff }) {
             <div className='month'>{tariff.month}</div>
 
           </div>
-
-
           <img src={tariff.hit} alt="" className='hit' />
         </div>
 
         <div className='block'>
 
-
           <div className='r-side'>
             <div className='traffic'>{tariff.traffic !== 0 && tariff.traffic}</div>
             <div classname='l-side'>
-            <div className='volium'>{tariff.volium !== 0 && tariff.volium} </div>
-            
-  
+              <div className='volium'>{tariff.volium !== 0 && tariff.volium} </div>
+
+
             </div>
           </div>
           <p className='text'>{tariff.text}</p>
@@ -39,7 +34,7 @@ export default function TariffItem({ tariff }) {
           <div className="img">{tariff.icon}</div>
           <span className='volue'>{tariff.value}</span>
         </div>
-        
+
         <div className='block'>
           <h1 className='.number' >{tariff.number !== 0 && tariff.number}</h1>
           <h3 className='time'>{tariff.time}</h3>
